467132dae6abdfe228f05e22a1ed2b16967be69d
Lionel-Feuga <feuga.lionel1@gmail.com>
2025-05-20 11:27:32 +0200

    t

--- screens/takePicScreen.tsx
+++ screens/takePicScreen.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Camera } from "expo-camera";
-import { useEffect } from "react";
-
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 const handleCapture = async () => {
   console.log("takepic");
 };
 export default function TakePicScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Take Picture</Text>
+       <Image style={styles.logo} source={require('../assets/logo.png')} />
+      <Text style={styles.text}>2 ways but only 1 result !</Text>
+      <FontAwesome name="faArrowUpFromBracket" size={25}/>
       <View style={styles.buttonContainer}>
         <Button title="Capture" onPress={handleCapture} />
       </View>
@@ -21,7 +21,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-around",
+    backgroundColor: "#2a2e30"
+
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    position: "absolute",
+    top: 25,
   },
   camera: {
     width: "100%",
@@ -30,6 +39,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: "bold",
+    color: "#fff",
   },
   buttonContainer: {
     marginTop: 20,
