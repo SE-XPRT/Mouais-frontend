975be4baa964906e46268f2b8eedce8015049558
SE-XPRT <meknaniimad@gmail.com>
2025-05-20 10:27:55 +0200

    push screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useNavigation } from "@react-navigation/native";
+import { useEffect } from "react";
+import DashboardScreen from "./screens/dashboardScreen";
+import SettingsScreen from "./screens/settingsScreen";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Mouais first page </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName = "";
+
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Settings") {
+              iconName = "user";
+            }
+
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#2196f3",
+          tabBarInactiveTintColor: "gray",
+          headerShown: false,
+        })}
+      >
+        <Tab.Screen name="Home" component={DashboardScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/dashboardScreen.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+export default function DashboardScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Dashboard</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
