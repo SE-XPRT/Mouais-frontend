5937719b924c04559c23051317be6aeed5d143b9
SE-XPRT <meknaniimad@gmail.com>
2025-05-20 17:26:21 +0200

    fin de journée

--- screens/takePicScreen.tsx
+++ screens/takePicScreen.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
+import _FontAwesome from "@react-native-vector-icons/fontawesome";
+const FontAwesome = _FontAwesome as React.ElementType;
 import { icon } from "@fortawesome/fontawesome-svg-core";
 
-const handleCapture = async ({ navigation }) => {
+const handleCapture = async () => {
   console.log("takepic");
 };
 
