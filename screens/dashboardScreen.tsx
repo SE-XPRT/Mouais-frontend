249eaa41ea87a8947f96c8d4f485bec163118570
Cecilia <cecilia.paulo@gmail.com>
2025-05-20 15:11:01 +0200

    dashboardScreen file creation

--- screens/dashboardScreen.tsx
+++ screens/dashboardScreen.tsx
@@ -1,11 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import _FontAwesome from "@react-native-vector-icons/fontawesome";
+const FontAwesome = _FontAwesome as React.ElementType;
+
 export default function DashboardScreen() {
-  const navigation = useNavigation({ navigation });
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Dashboard</Text>
+      <Image style={styles.logo} source={require("../assets/logo.png")} />
+      <Text style={styles.greetings}>
+        Salut Juju, prêt à tester ton <Text style={styles.aura}>aura</Text> ?
+      </Text>
+
+      <View style={styles.cardGrid}>
+        <TouchableOpacity style={styles.card}>
+          <FontAwesome name="web-awesome" size={40} color="#29ffc6" />
+          <Text style={styles.cardText}>Tes badges</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <FontAwesome name="thumbs-up" size={40} color="#ffac25" />
+          <Text style={styles.cardText}>Ton best score</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <FontAwesome name="images" size={40} color="#8b43f1" />
+          <Text style={styles.cardText}>Ta best photo</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.card}>
+          <FontAwesome name="bitcoin" size={40} color="#ff0084" />
+          <Text style={styles.cardText}>Tes coins</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.pinkButton}>
+          <Text style={styles.buttonText}>Gagne des coins</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.greenButton}>
+          <Text style={styles.buttonText}>BUY PREMIUM</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.coinInfo}> Il te reste 3 coins</Text>
+      </View>
     </View>
   );
 }
@@ -13,11 +59,65 @@ export default function DashboardScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#2a2e30",
     alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 60,
   },
-  text: {
-    fontSize: 24,
+  logo: {
+    width: 120,
+    height: 60,
+    resizeMode: "contain",
+    marginBottom: 10,
+  },
+  greetings: {
+    color: "#fff",
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  aura: {
+    color: "#E7C6FF",
     fontWeight: "bold",
+  },
+  cardGrid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: "#383c40",
+    width: 130,
+    height: 100,
+    margin: 10,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardText: {
+    color: "#fff",
+    fontSize: 14,
+    marginTop: 10,
+    textAlign: "center",
+  },
+  pinkButton: {
+    backgroundColor: "#ff0084",
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  greenButton: {
+    backgroundColor: "#29ffc6",
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#000",
+    fontWeight: "bold",
+  },
+  coinInfo: {
+    color: "#fff",
+    marginTop: 20,
   },
 });
